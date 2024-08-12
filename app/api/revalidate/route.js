2374633c009d5955f0ed225cf83904415c541ba2
palatiumdev <palatiumwebdesign@gmail.com>
2024-08-12 02:54:15 +0200

    onDemandISR

---
+++ app/api/revalidate/route.js
@@ -0,0 +1,31 @@
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
+
+const secret = process.env.MY_WEBHOOK_SECRET
+
+export default async function handler(req, res) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME]
+  const body = await readBody(req) // Read the body into a string
+  if (!(await isValidSignature(body, signature, secret))) {
+    res.status(401).json({ success: false, message: 'Invalid signature' })
+    return
+  }
+
+  const jsonBody = JSON.parse(body)
+  doSomeMagicWithPayload(jsonBody)
+  res.json({ success: true })
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+async function readBody(readable) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
+}
